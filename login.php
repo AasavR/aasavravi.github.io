88f827b43a5bcded4e4c1dd1c90bec1f851bd290
ASAV R <aasavravi@gmail.com>
2021-10-22 22:34:20 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,13 @@
-<?php
+<html>
+<body>
 
-var_dump($_POST);
+<?php echo ($_POST)["inp_name"]; ?>
+
+<?php echo $_POST["inp_email"]; ?>
+
+<?php echo $_POST["flexRadioDefault1"]; ?>
+
+<?php echo $_POST["flexRadioDefault2"]; ?>
+  
+</body>
+</html>
