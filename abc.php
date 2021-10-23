d391d925d6a4a4185546d9a5b5cbcd036d942568
ASAV R <aasavravi@gmail.com>
2021-10-23 11:10:29 +0530

    Update abc.php

--- abc.php
+++ abc.php
@@ -4,7 +4,7 @@
   <?php
   $filename = "http://www.lasavo.org/login.php";
   fopen( $filename, "r");
-     $content =  name"inp_name", email"inp_email", yes"flexRadioDefault1", no"flexRadioDefault2";
+     $content =  name , email, yes, no;
   fread($content)
     fwrite ( "http://www.lasavo.org/abc.php", $content)
     fclose($handle);
@@ -12,3 +12,4 @@
      
      </body>
 </html>
+     
