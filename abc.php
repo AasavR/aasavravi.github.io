96dff75b1580a74a1331c3a1f028b475794a461a
ASAV R <aasavravi@gmail.com>
2021-10-23 11:06:17 +0530

    Update abc.php

--- abc.php
+++ abc.php
@@ -3,33 +3,12 @@
      
   <?php
   $filename = "http://www.lasavo.org/login.php";
-  $handle = fopen( $filename, "r");
-  $contents = fread($handle, filesize($filename));
- <?php if (isset($_POST["inp_name"]))
-{
-  $d = $_POST["inp_name"];
-  echo($d);
-}
-   ?>
-  <?php  if (isset($_POST["inp_email"]))
-{
-  $d = $_POST["inp_email"];
-  echo($d);
-}
-   ?>
- <?php if (isset($_POST["flexRadioDefault1"]))
-{
-  $d = $_POST["flexRadioDefault1"];
-  echo($d);
-}
-   ?>
-  <?php if (isset($_POST["flexRadioDefault2"]))
-{
-  $d = $_POST["flexRadioDefault2"];
-  echo($d);
-}
-  
-  fread( name"inp_name", email"inp_email", yes"flexRadioDefault1", no"flexRadioDefault2")
-    fwrite ( name"inp_name", email"inp_email", yes"flexRadioDefault1", no"flexRadioDefault2")
+  fopen( $filename, "r");
+     $content =  name"inp_name", email"inp_email", yes"flexRadioDefault1", no"flexRadioDefault2";
+  fread($content)
+    fwrite ( "http://www.lasavo.org/abc.php", $content)
     fclose($handle);
   ?>
+     
+     </body>
+</html>
