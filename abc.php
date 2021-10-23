9786b5366d53687ca092e2ca805ce2b7cc40f692
ASAV R <aasavravi@gmail.com>
2021-10-23 21:18:17 +0530

    Update abc.php

--- abc.php
+++ abc.php
@@ -5,6 +5,7 @@
   $filename = "http://www.lasavo.org/login.php";
   fopen( $filename, "r");
      $content =  ['$name' , '$email', '$yes', '$no', '$comment'];
+     echo $_POST["inp_name", "inp_email", "flexRadioDefault1", "flexRadioDefault2", "inp_comments"];
   fread($content)
      $filename2 =  fopen ( "http://www.lasavo.org/abc.php", "w+");
     fwrite ( $filename2 , $content)
