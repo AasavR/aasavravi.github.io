31f4fc61ba0044c1c47a2512f5ea72ab40562f5e
aasavravi <aasavravi@gmail.com>
2021-10-25 16:16:02 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,20 +1,26 @@
-
+<!DOCTYPE html>
+<html>
+ <body>
  <?php 
   
           if ( isset($_POST['submit'])) {
-            $name = $_POST["name"];
-            $email = $_POST["email"];
-            $yes = $_POST["flexRadioDefault1"];
-            $no = $_POST["flexRadioDefault2"];
-            $comment= $_POST["comment"];
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $yes = $_POST['flexRadioDefault1'];
+            $no = $_POST['flexRadioDefault2'];
+            $comment= $_POST['comment'];
            
             echo 'Thank You for your time';
           }
             
-            $fp = fopen('data.txt', 'w');//open file in write mode  
-            fwrite($fp, ['$name','$email', '$yes', '$no', '$comment']);  
+            $fp = fopen('data.txt', 'w'); 
+            $txt =  ('$name', '$email', '$yes", '$no', '$comment');
+            fwrite($fp, $txt);  
             fclose($fp);  
   
             echo "File written successfully";  
             
   ?>
+  
+ </body>
+</html>
